bf68d92276b460a8266b539decf6b5d034edd8ab
Katya Laso <kat_jenny@hotmail.com>
2022-11-22 12:38:37 -0600

    agrega componente pedidos

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,21 +3,32 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 //import Login from './componentes/LogIn.js'
 import {Header, titulo} from './componentes/Header.js'
+import BotonesMesero from './componentes/BotonesMesero';
+import Pedido from './componentes/Pedidos'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
     <Header
       text = { titulo.text}
-    />
-    {/*  <Header
+       />
+    <BotonesMesero/>
+    <Pedido/>
+   
+    {/*
+      <Login/>
+
+      <Header
+      text = { titulo.text}
+       />
+    
+    <Header
       text = { titulo.text= "otro texto"}
     /> */}
   </React.StrictMode>
 );
-//<Login/>
+//
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
