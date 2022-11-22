96a1788d0897a0e74ede94efb3366fcf78f4723e
vanesadeneb <vanessa_diesver@hotmail.com>
2022-11-21 23:13:32 -0700

    feat: crea el componente de Pedido y estilo css

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import Login from './componentes/LogIn.js'
-
+//import Login from './componentes/LogIn.js'
+import Pedido from './componentes/pedidos.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login/>
+    <Pedido/>
   </React.StrictMode>
 );
 
