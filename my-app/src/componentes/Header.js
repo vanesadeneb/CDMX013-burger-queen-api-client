1d8268a7214f95ea686bb2aa59caa6421df0599c
vanesadeneb <vanessa_diesver@hotmail.com>
2022-11-22 20:34:33 -0700

    feat: agregar react-router para salir de la app desde la vista de meseros en el componente de Header.js

--- my-app/src/componentes/Header.js
+++ my-app/src/componentes/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import spaceburgersHorizontal from '../imagenes/spaceburgersHorizontal.png';
-import rocket from '../imagenes/rocket.png'
+import rocket from '../imagenes/rocket.png';
+import { useNavigate } from 'react-router-dom';
 
 export const Header = (props) => {
+    const navigate = useNavigate();
+    
+    const logout = (e) => {
+        navigate('/');
+    } 
     return(
         <div className="header-container">
             <img className="logoHorizontal" src={spaceburgersHorizontal} alt="Space Burgers" />
             <p className="titulo">{props.text}</p>
-            <div className='contenedor-logout'>
+            <div className='contenedor-logout' onClick={logout}>
                 <img className="logOut" src={rocket} alt="Log Out" />
                 <p className='logOutText'>Log Out</p>
             </div>
