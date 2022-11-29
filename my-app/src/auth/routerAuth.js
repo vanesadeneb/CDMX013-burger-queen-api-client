46120396db36c27d327ee2004fc63a70b9b30546
vanesadeneb <vanessa_diesver@hotmail.com>
2022-11-29 09:02:58 -0700

    feat: agrega cambios para usar rutas para cargar dinamicamente productos de la mock api

--- my-app/src/auth/routerAuth.js
+++ my-app/src/auth/routerAuth.js
@@ -3,22 +3,18 @@ import {
     createBrowserRouter,
   } from "react-router-dom";
 import Mesero from "../componentes/Mesero";
-import Admin from '../componentes/Admin';
+import ProductosBreakfast from '../componentes/ProductosDesayuno';
 
 export const routerAuth = createBrowserRouter([
     {
       path: "/pedidos",
       element: <Mesero />,
-      /*
+      
       children: [
         {
-            path: "/usuarios",
-            element: <Usuarios />,
+            path: "pedidos/menuDesayuno",
+            element: <ProductosBreakfast />,
         }
-      ],*/
-    },
-    {
-      path : "/admin",
-      element : <Admin />,
+      ],
     }
   ]);
